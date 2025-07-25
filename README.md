111036b1345236388201617e8277c1aa62e1b99c
Christelle Souka <soukachristelle@gmail.com>
2025-07-25 10:13:30 +0200

    Manipulez vos données

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cours OpenClassrooms: Apprenez à programmer en Java
+
+
+## Objectifs
+
+- Maîtriser les bases de Java
+- Prendre en main la programmation orientée objet
+- Approfondir sa maîtrise de Java
+
+## Fonctionnalités principales
+
+- À venir
--- src/LearnJava/Main.java
+++ src/LearnJava/Main.java
@@ -3,7 +3,15 @@ package LearnJava;
 public class Main {
 
 	public static void main(String[] args) {
-		Main.affichage("Marty");
+		int[] tableau = {26, 10, 1985, 0, 12, 11, 1955};
+		int cpt = 0;
+		
+		for (int entier : tableau) {
+			if (entier == 0) {
+				cpt++;
+			}
+		}
+		System.out.println(cpt);
 	}
 	
 	public static void affichage(final String text) {
