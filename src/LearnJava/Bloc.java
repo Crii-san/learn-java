143b9915570f1b1ebac841723b1d2a5966652094
Christelle Souka <soukachristelle@gmail.com>
2025-08-16 09:43:46 +0200

    Ecrivez votre première classe

---
+++ src/LearnJava/Bloc.java
@@ -0,0 +1,26 @@
+package LearnJava;
+
+public class Bloc {
+	
+	private int longueur;
+	private int largeur;
+	private int hauteur;
+	
+	Bloc(final int longueur, final int largeur, final int hauteur) {
+		this.longueur = longueur;
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+	}
+
+	public int getLongueur() {
+		return longueur;
+	}
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+	public int getHauteur() {
+		return hauteur;
+	}
+}
--- src/LearnJava/Main.java
+++ src/LearnJava/Main.java
@@ -3,18 +3,6 @@ package LearnJava;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] tableau = {26, 10, 1985, 0, 12, 11, 1955};
-		int cpt = 0;
-		
-		for (int entier : tableau) {
-			if (entier == 0) {
-				cpt++;
-			}
-		}
-		System.out.println(cpt);
-	}
-	
-	public static void affichage(final String text) {
-		System.out.println("Bonjour " + text);
+
 	}
 }
